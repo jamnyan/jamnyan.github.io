19177d6fb0ca044e8192649910e17de22ef4bb6b
jam yan <jamnyan4683@gmail.com>
2021-08-09 19:29:39 +0800

    猪的生日祝福

---
+++ ok/js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="张佳瑶" &&  pwd=="959959"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("不对嗷，再试试");
+	}
+});
