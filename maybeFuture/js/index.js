0752f1e789138fc97f11cfe43dc40cc551fa0a40
jam yan <jamnyan4683@gmail.com>
2024-11-07 17:50:43 +0800

    新效果

--- maybeFuture/js/index.js
+++ maybeFuture/js/index.js
@@ -9,8 +9,29 @@ parallax.addEventListener('scroll', function() {
         // console.log('layer', layer, index);
         const speed = (index-6) / 15
         layer.style.transform = `translateY(${-(scrollTop * speed)}px)`
-        if(index == 6){
-            // showBox.style.transform = `translateY(${-(scrollTop * speed)}px)`
-        }
+        // if(index == 6){
+        //     showBox.style.transform = `translateY(${-(scrollTop * speed)}px)`
+        // }
     })
 })
+
+
+// 首页时间部分
+const time = document.querySelector('.time');
+
+function showTime(dom) {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours()<10?'0'+date.getHours():date.getHours();
+    const minute = date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes();
+    const second = date.getSeconds()<10?'0'+date.getSeconds():date.getSeconds();
+    // dom.innerHTML = `${year}年${month}月${day}日 ${hour}:${minute}:${second}`;
+    dom.innerHTML = `${hour}:${minute}:${second}`;
+}
+
+showTime(time);
+setInterval(function() {
+    showTime(time);
+}, 1000);
